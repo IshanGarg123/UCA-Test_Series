4bbe32abe525068f3eb15205d44a442aac633f08
Ishan Garg <sonuaarti1978@gmail.com>
2023-12-21 11:01:18 +0530

    bits and bytes file added

--- Test1/BitsAndBytes.c
+++ Test1/BitsAndBytes.c
@@ -8,10 +8,15 @@ int bitXor (int x, int y) {
     return ~(~(x & ~y) & ~(~x & y));
 }
 
+int sign(int x) {
+    return (!!x) | (x >> 31);
+}
+
 
 int main()
 {
     printf("%d %d %d %d\n",bitAnd(0,0),bitAnd(1,0),bitAnd(0,1),bitAnd(1,1));
     printf("%d %d %d %d\n",bitXor(0,0),bitXor(1,0),bitXor(0,1),bitXor(1,1));
+    printf("%d %d %d\n",sign(102),sign(0),sign(-111));
     return 0;
 }
